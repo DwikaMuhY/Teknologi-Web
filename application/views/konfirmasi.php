f5673bbdabd5508fc04568e38603e7905aa21a8f
Fuzies <85861723+Fuzies@users.noreply.github.com>
2021-06-14 14:56:46 +0700

    Update konfirmasi.php
    header link ke view/konfirmasi dan view/calon

--- application/views/konfirmasi.php
+++ application/views/konfirmasi.php
@@ -8,32 +8,30 @@
     <link rel="stylesheet" href="<?php echo site_url('../assets/css/bootstrap.css')?>">
 </head>
 <body>
-	<header>
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<a class="navbar-brand" href="#">Admin</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarNav">
-				<ul class="navbar-nav">
-					<li class="nav-item active">
-						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Features</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Pricing</a>
-					</li>
-				</ul>
-				<ul class="nav navbar-nav ml-auto">
-					<li class="nav-item">
-						<a class="nav-link" href="">Logout</a>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	</header>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+	  <a class="navbar-brand" href="#">Halaman Admin</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarText">
+
+	    <ul class="navbar-nav mr-auto">
+	      <li>
+	        <?php
+	      	$calon = '<a class="nav-link" href="'.site_url("Home/pindahKonfirm/").'">Konfirmasi</a>';
+	        echo $calon;
+	        ?>
+	      </li>
+	      <li class="nav-item">
+	      <?php
+	      	$calon = '<a class="nav-link" href="'.site_url("Home/pindahCalon/").'">Calon</a>';
+	        echo $calon;
+	        ?>
+	      </li>
+	    </ul>
+
+	  </div>
+	</nav>
 	<table border="1" align="center">
 			<tr>
 				<th>NIK</th><th>Nama</th><th>Email</th><th>Akses</th>
