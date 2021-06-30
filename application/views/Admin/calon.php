8ec1223de1d1cfacd95a03955b88f31f5a9f3337
imanmuhdi <85862742+imanmuhdi@users.noreply.github.com>
2021-06-30 22:04:26 +0700

    Perubahan file calon untuk tampil data calon

--- application/views/Admin/calon.php
+++ application/views/Admin/calon.php
@@ -4,21 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calon</title>
+    <title>Calon Presiden</title>
     <link rel="stylesheet" href="<?php echo site_url('../assets/css/bootstrap.css')?>">
+	<link rel="stylesheet" href="http://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="http://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
     <style>
-		  .header {
-		  padding: 60px;
-		  text-align: center;
-		  background: #1abc9c;
-		  color: white;
-		  font-size: 30px;
+		.content-wrapper{
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			padding-top: 40px;
 		}
-		
 		.logout{
 			color: grey;
 		}
-
 		.logout:hover{
 			text-decoration: none;
 			color: rgb(200,200,200);
@@ -37,14 +37,14 @@
 				<ul class="navbar-nav mr-auto">
 				<li>
 					<?php
-					$calon = '<a class="nav-link" href="'.site_url("Admin/pindahKonfirm/").'">Konfirmasi</a>';
-					echo $calon;
+					$calon1 = '<a class="nav-link" href="'.site_url("Admin/pindahKonfirm/").'">Konfirmasi</a>';
+					echo $calon1;
 					?>
 				</li>
 				<li class="nav-item">
 				<?php
-					$calon = '<a class="nav-link" href="'.site_url("Admin/pindahCalon/").'">Calon</a>';
-					echo $calon;
+					$calon1 = '<a class="nav-link" href="'.site_url("Admin/pindahCalon/").'">Calon</a>';
+					echo $calon1;
 					?>
 				</li>
 				</ul>
@@ -57,6 +57,32 @@
 			</div>
 		</div>
 	</nav>
-	Isi dari Calon Disini
+	<div class="container" style="padding-top:40px; padding-left: 999px">
+      		<a class="btn btn-primary" href="<?php echo site_url('Admin/tambahCalon');?>">Tambah Calon</a>
+   	</div>
+	<div class="content-wrapper">
+   		<div class="container">
+      		<h3>Daftar Calon Presiden</h3>
+      		<?php
+				$template = array( 
+					'table_open' => '<table id="myTable" border=1>' 
+				);
+				$this->table->set_template($template); 
+				$this->table->set_heading("Nama Calon","Foto","Visi","Misi","Aksi"); 
+				foreach($calon->result() as $r){
+					$edit = '<a href="'.site_url("Admin/edit/".$r->id_calon).'" class="btn btn-primary">Edit</a>';
+					$hapus = '<a href="'.site_url("Admin/hapus/".$r->id_calon).'" class="btn btn-danger">Hapus</a>';
+					$aksi = "<div class='d-flex justify-content-between align-items-start'>".$edit.$hapus."</div>";
+					$this->table->add_row($r->nama_calon,$r->foto,$r->visi,$r->misi,$aksi);
+				}
+				echo $this->table->generate();
+			?>
+   		</div>
+	</div>
 </body>
+<script>
+	$(document).ready(function() {
+		$('#myTable').DataTable(); 
+	});
+</script>
 </html>
